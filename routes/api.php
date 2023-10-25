bfd0a305a9aa835a5d7af1c68b4a1b459854fb41
gwencelot <seangwenallare@gmail.com>
2023-10-25 18:19:11 +0800

    Created controller setup api and seeded db

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\CarouselItemsController;
+use App\Models\CarouselItems;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/carousel', [CarouselItemsController::class, 'index']);
+
